629e9b55f23f182e0c40df806a1a2191d9c8b130
Klaas <klaas@users.noreply.github.com>
2018-12-30 16:28:33 +0100

    guardIs range

--- Sources/Guaka/Command/Command+ParkBench.swift
+++ Sources/Guaka/Command/Command+ParkBench.swift
@@ -8,7 +8,12 @@
 
 
 extension Command {
+	@available(*, deprecated, message:"user guard(countIs:statusCode:)")
 	public func `guard`(_ args:[String], countInRange range:CountableClosedRange<Int>, statusCode: Int = -1) {
+		self.guard(args, countIs: range, statusCode: statusCode)
+	}
+	
+	public func `guard`(_ args:[String], countIs range:CountableClosedRange<Int>, statusCode: Int = -1) {
 		guard case range = args.count else {
 			if args.count < range.lowerBound {
 				print("Too few arguments.")
@@ -16,10 +21,11 @@ extension Command {
 				print("Too many arguments.")
 			}
 			
-//			print( helpMessage)
+			//			print( helpMessage)
 			fail(statusCode: statusCode)
 		}
 	}
+	
 	public func `guard`(_ args:[String], countIs count:Int, statusCode: Int = -1) {
 		guard count == args.count else {
 			if args.count < count {
@@ -28,7 +34,7 @@ extension Command {
 				print("Too many arguments.")
 			}
 			
-//			print( helpMessage)
+			//			print( helpMessage)
 			fail(statusCode: statusCode)
 		}
 	}
