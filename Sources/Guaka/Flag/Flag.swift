4f91d22fbf6367fb2ca78b9eda855cafd23ac54c
klaas <m@klaas.is>
2019-03-26 20:08:17 +0100

    Swift 5

--- Sources/Guaka/Flag/Flag.swift
+++ Sources/Guaka/Flag/Flag.swift
@@ -88,9 +88,9 @@ public struct Flag: Hashable {
 
 
   /// Gets the flag hash value.
-  public var hashValue: Int {
-    return longName.hashValue
-  }
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(longName)
+	}
 
 
   /// Return if the flag is a boolean or not.
