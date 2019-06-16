494c171332a8694605a5384089a9fcd6fbbf9af7
klaas <m@klaas.is>
2019-06-16 20:43:38 +0200

    mini

--- Sources/Guaka/Command/Command+ParkBench.swift
+++ Sources/Guaka/Command/Command+ParkBench.swift
@@ -3,11 +3,6 @@
 import Foundation
 
 extension Command {
-	@available(*, deprecated, message:"user guard(countIs:statusCode:)")
-	public func `guard`(_ args:[String], countInRange range:CountableClosedRange<Int>, statusCode: Int = -1) {
-		self.guard(args, countIs: range, statusCode: statusCode)
-	}
-	
 	public func `guard`(_ args:[String], countIs range:CountableClosedRange<Int>, statusCode: Int = -1) {		guard case range = args.count else {
 			if args.count < range.lowerBound {
 				print("Too few arguments.")
